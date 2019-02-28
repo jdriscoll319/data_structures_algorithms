fb29994c4b778cd1cd74b84f9d0902157c7f820b
Adam Driscoll <jdriscol@andrew.cmu.edu>
2019-02-28 15:17:59 -0800

    quicksort and quicksort3 implemented

--- week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
+++ week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
@@ -1,40 +1,80 @@
 #include <iostream>
 #include <vector>
-#include <cstdlib>
+#include <utility>
+#include <random>
+#include <chrono>
 
-using std::vector;
-using std::swap;
-
-int partition2(vector<int> &a, int l, int r) {
-  int x = a[l];
-  int j = l;
-  for (int i = l + 1; i <= r; i++) {
-    if (a[i] <= x) {
-      j++;
-      swap(a[i], a[j]);
+size_t partition2(std::vector<int> &a, size_t left, size_t right)
+{
+  int pivot = a[left];
+  size_t greater_pos = left + 1;
+  for (size_t i = left + 1; i <= right; ++i)
+  {
+    if (a[i] <= pivot)
+    {
+      std::swap(a[i], a[greater_pos++]);
     }
   }
-  swap(a[l], a[j]);
-  return j;
+  std::swap(a[left], a[--greater_pos]);
+
+  return greater_pos;
 }
 
-void randomized_quick_sort(vector<int> &a, int l, int r) {
-  if (l >= r) {
-    return;
+void partition3(std::vector<int> &a, size_t left, size_t right, size_t &ml, size_t &mr)
+{
+  int pivot = a[left];
+  // std::cout << "Partitioning:\n   left: " << left << " | right: " << right << " | pivot: " << pivot << "\n";
+  size_t eq_pos = left;
+  size_t gt_pos = left + 1;
+  for (size_t i = left + 1; i <= right; ++i)
+  {
+    if (a[i] < pivot)
+    {
+      std::swap(a[i], a[eq_pos++]);
+      std::swap(a[i], a[gt_pos++]);
+    }
+    else if (a[i] == pivot)
+    {
+      std::swap(a[i], a[gt_pos++]);
+    }
   }
+  // std::swap(a[left], a[--eq_pos]);
 
-  int k = l + rand() % (r - l + 1);
-  swap(a[l], a[k]);
-  int m = partition2(a, l, r);
+  // for (auto val : a)
+  //   std::cout << val << ' ';
+  // std::cout << "\n";
 
-  randomized_quick_sort(a, l, m - 1);
-  randomized_quick_sort(a, m + 1, r);
+  ml = eq_pos;
+  mr = gt_pos;
+}
+
+void randomized_quick_sort(std::vector<int> &a, int left, int right)
+{
+  if (left > right)
+    return;
+  
+  std::default_random_engine generator(std::chrono::system_clock::now().time_since_epoch().count());
+  std::uniform_int_distribution<int> distribution(left, right);
+  size_t pivot = distribution(generator);
+  // std::cout << "Left: " << left << " Right: " << right << "\n";
+  // std::cout << "random pivot: " << pivot << ' ' << a[pivot] << "\n";
+
+  std::swap(a[left], a[pivot]);
+
+  size_t ml, mr;
+
+  // size_t mid = partition2(a, left, right);
+  partition3(a, left, right, ml, mr);
+  // std::cout << "sorting left\n";
+  randomized_quick_sort(a, left, ml -1);
+  // std::cout << "sorting right\n";
+  randomized_quick_sort(a, mr, right);
 }
 
 int main() {
   int n;
   std::cin >> n;
-  vector<int> a(n);
+  std::vector<int> a(n);
   for (size_t i = 0; i < a.size(); ++i) {
     std::cin >> a[i];
   }
@@ -42,4 +82,5 @@ int main() {
   for (size_t i = 0; i < a.size(); ++i) {
     std::cout << a[i] << ' ';
   }
+  std::cout << "\n";
 }
